39fc5a75f8f477c9eac6f350f3b87af99aa0d152
OGEmanuel <emanuolumide@gmail.com>
2023-10-01 23:40:56 +0100

    Initial Commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for i := range numbers {
+		if numbers[i]%2 == 0 {
+			fmt.Println("even")
+		} else {
+			fmt.Println("odd")
+		}
+	}
+
+	// OR
+
+	for _, number := range numbers {
+		if number%2 == 0 {
+			fmt.Println(number, "is even")
+		} else {
+			fmt.Println(number, "is odd")
+		}
+	}
+}
